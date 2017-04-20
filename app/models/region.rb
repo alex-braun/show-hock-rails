4c7b3fce075ec88a34e779c91c9eda853d1d7435
alex-braun <braunacb@gmail.com>
2017-04-20 19:11:23 -0400

    Fixing implicit string conversion error
    Error only exists in deployed app.

--- app/models/region.rb
+++ app/models/region.rb
@@ -7,16 +7,16 @@ class Region
     @per_page = params.fetch(:per_page)
     @min_date = '&min_date=' + params.fetch(:min_date).to_s
     @max_date = '&max_date=' + params.fetch(:max_date).to_s
-    if params.fetch(:min_date) == ''
-      @min_date = ''
+    if params.fetch(:min_date).to_s == ''
+      @min_date = '&'
     end
-    if params.fetch(:max_date) == ''
-      @max_date = ''
+    if params.fetch(:max_date).to_s == ''
+      @max_date = '&'
     end
     @body = Unirest.get((
-    'http://api.songkick.com/api/3.0/metro_areas/' + @id.to_s +
-    '/calendar.json?page=' + @page.to_s + '&per_page=' +
-    @per_page.to_s + @min_date + @max_date + '&apikey=' + ENV['songkick_key']),
+    "http://api.songkick.com/api/3.0/metro_areas/" + @id.to_s +
+    "/calendar.json?page=" + @page.to_s + "&per_page=" +
+    @per_page.to_s + @min_date + @max_date + "&apikey=" + ENV['songkick_key']),
     headers: {
       'Accept' => 'application/json'
     }).body
