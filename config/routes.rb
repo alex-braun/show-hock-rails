6ca562363855b7a144694009ee4b3070f317c0a2
alex-braun <braunacb@gmail.com>
2017-04-22 12:00:17 -0400

    Added regEx in routes to allow '.''s in IDs
    `get '/artist_searches/:id' => 'artist_searches#show',
    :constraints => { :id => /[^\/]+/ }`

--- config/routes.rb
+++ config/routes.rb
@@ -10,11 +10,13 @@ Rails.application.routes.draw do
   resources :upcoming_events, except: [:new, :edit]
   resources :similar_artists, except: [:new, :edit]
   resources :artists, except: [:new, :edit]
-  resources :artist_searches, except: [:new, :edit]
+  # resources :artist_searches, except: [:new, :edit]
+  # resources :artist_searches, :id => /[A-Za-z0-9\.]+?/
   resources :regions, except: [:new, :edit]
   resources :region_searches, except: [:new, :edit]
   resources :events, except: [:new, :edit]
   resources :examples, except: [:new, :edit]
+  get '/artist_searches/:id' => 'artist_searches#show', :constraints => { :id => /[^\/]+/ }
   post '/sign-up' => 'users#signup'
   post '/sign-in' => 'users#signin'
   delete '/sign-out/:id' => 'users#signout'
